9f0a8214eaec1e0309dc307d1a6cc96c4cabbe5f
BeerDeveloper <castellanogiovanni97@gmail.com>
2024-03-23 11:50:55 +0100

    Repo reupload to clean commits

---
+++ StuntGPEditor/StuntGPEditor/Form1.Designer.cs
@@ -0,0 +1,123 @@
+﻿namespace StuntGPEditor
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variabile di progettazione necessaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Pulire le risorse in uso.
+        /// </summary>
+        /// <param name="disposing">ha valore true se le risorse gestite devono essere eliminate, false in caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Codice generato da Progettazione Windows Form
+
+        /// <summary>
+        /// Metodo necessario per il supporto della finestra di progettazione. Non modificare
+        /// il contenuto del metodo con l'editor di codice.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.mainDataGrid = new System.Windows.Forms.DataGridView();
+            this.buttonOpen = new System.Windows.Forms.Button();
+            this.buttonSave = new System.Windows.Forms.Button();
+            this.consoleRichTextBox = new System.Windows.Forms.RichTextBox();
+            this.buttonHelp = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.mainDataGrid)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // mainDataGrid
+            // 
+            this.mainDataGrid.BackgroundColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.mainDataGrid.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.mainDataGrid.EditMode = System.Windows.Forms.DataGridViewEditMode.EditOnEnter;
+            this.mainDataGrid.GridColor = System.Drawing.SystemColors.Desktop;
+            resources.ApplyResources(this.mainDataGrid, "mainDataGrid");
+            this.mainDataGrid.MultiSelect = false;
+            this.mainDataGrid.Name = "mainDataGrid";
+            this.mainDataGrid.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            // 
+            // buttonOpen
+            // 
+            this.buttonOpen.Cursor = System.Windows.Forms.Cursors.Hand;
+            resources.ApplyResources(this.buttonOpen, "buttonOpen");
+            this.buttonOpen.Name = "buttonOpen";
+            this.buttonOpen.UseVisualStyleBackColor = true;
+            this.buttonOpen.Click += new System.EventHandler(this.buttonOpen_Click);
+            // 
+            // buttonSave
+            // 
+            this.buttonSave.Cursor = System.Windows.Forms.Cursors.Hand;
+            resources.ApplyResources(this.buttonSave, "buttonSave");
+            this.buttonSave.Name = "buttonSave";
+            this.buttonSave.UseVisualStyleBackColor = true;
+            this.buttonSave.Click += new System.EventHandler(this.buttonSave_Click);
+            // 
+            // consoleRichTextBox
+            // 
+            this.consoleRichTextBox.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.consoleRichTextBox.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.consoleRichTextBox.CausesValidation = false;
+            this.consoleRichTextBox.Cursor = System.Windows.Forms.Cursors.Arrow;
+            this.consoleRichTextBox.ForeColor = System.Drawing.Color.White;
+            resources.ApplyResources(this.consoleRichTextBox, "consoleRichTextBox");
+            this.consoleRichTextBox.Name = "consoleRichTextBox";
+            this.consoleRichTextBox.ReadOnly = true;
+            // 
+            // buttonHelp
+            // 
+            this.buttonHelp.Cursor = System.Windows.Forms.Cursors.Help;
+            resources.ApplyResources(this.buttonHelp, "buttonHelp");
+            this.buttonHelp.Name = "buttonHelp";
+            this.buttonHelp.UseVisualStyleBackColor = true;
+            this.buttonHelp.Click += new System.EventHandler(this.buttonHelp_Click);
+            // 
+            // pictureBox1
+            // 
+            resources.ApplyResources(this.pictureBox1, "pictureBox1");
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            resources.ApplyResources(this, "$this");
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ControlText;
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.buttonHelp);
+            this.Controls.Add(this.consoleRichTextBox);
+            this.Controls.Add(this.buttonSave);
+            this.Controls.Add(this.buttonOpen);
+            this.Controls.Add(this.mainDataGrid);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.mainDataGrid)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        public System.Windows.Forms.DataGridView mainDataGrid;
+        public System.Windows.Forms.Button buttonOpen;
+        public System.Windows.Forms.Button buttonSave;
+        public System.Windows.Forms.RichTextBox consoleRichTextBox;
+        public System.Windows.Forms.Button buttonHelp;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
